d667fbd39561e8138169bad0525905e6a7dde403
sameer-khan-a <sameerkhan003a@gmail.com>
2024-12-28 01:33:02 +0530

    add update functionality

--- index.js
+++ index.js
@@ -1,35 +1,61 @@
 import express from 'express';
 
 const app = new express();
-const port = 3002;
-var posts = [];
+const port = 4000;
+let posts = [
+    {
+      id: 1,
+      title: "The Rise of Decentralized Finance",
+      content:
+        "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
+      author: "Alex Thompson",
+      date: "2023-08-01T10:00:00Z",
+    },
+    {
+      id: 2,
+      title: "The Impact of Artificial Intelligence on Modern Businesses",
+      content:
+        "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
+      author: "Mia Williams",
+      date: "2023-08-05T14:30:00Z",
+    },
+    {
+      id: 3,
+      title: "Sustainable Living: Tips for an Eco-Friendly Lifestyle",
+      content:
+        "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
+      author: "Samuel Green",
+      date: "2023-08-10T09:15:00Z",
+    },
+  ];
+  let oldId = posts.length;
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}));
+  
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}.`)
-})
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.render('index.ejs', {
-        title: 'Home',
-        posts: posts
-    });
-})
-
-app.get('/create', (req, res) => {
-    res.render('create.ejs', {
-        title: "Add a Post",
-    });
-})
+app.get('/posts', (req, res) => {
+    res.json(posts);
+  })
 
 app.post('/create', (req, res) => {
-    posts.push(req.body);
-    res.redirect('/');
+  var newId = oldId + 1;
+    var newPost = {
+      id: newId,
+      author: req.body.author,
+      title: req.body.title,
+      content: req.body.content
+    }
+    newId+=1;
+    posts.push(newPost);
+    res.json(newPost);
 })
-
+app.get('/posts/:id', (req, res) => {
+    var id = parseInt(req.params.id);
+    var post = posts.find((post) => post.id === id);
+    console.log(post);
+    post?res.json(post):res.status(404).json({error: 'post does not exist'});
+  })
 app.delete('/Posts/:title', (req, res) => {
     const title = req.params.title;
     const initialLength = posts.length;
@@ -42,3 +68,13 @@ app.delete('/Posts/:title', (req, res) => {
     }
 });
 
+app.patch('/posts/:id', (req, res) => {
+    var currentPost = posts.find((p) => p.id === parseInt(req.params.id));
+      req.body.title?currentPost.title = req.body.title:currentPost.title;
+      req.body.content?currentPost.content = req.body.content:currentPost.content;
+      req.body.author?currentPost.author = req.body.author:currentPost.author;
+    currentPost?res.json(currentPost):res.status(404).res.json({error: 'post does not exist'});
+    })
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}.`)
+    })
---
+++ server.js
@@ -0,0 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "http://localhost:4000";
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.get('/', async (req, res) => {
+    const response = await axios.get(`${API_URL}/posts`);
+    res.render('index.ejs', {
+        title: 'Home',
+        posts: response.data
+    });
+})
+
+app.get('/create', (req, res) => {
+    res.render('create.ejs', {
+        heading: "Add a Post",
+    });
+})
+
+app.get("/posts/:id", async (req, res) => {
+    try {
+        const response = await axios.get(`${API_URL}/posts/${parseInt(req.params.id)}`);
+      console.log(response.data);
+      res.render("modify.ejs", {
+        heading: "Edit Post",
+        submit: "Update Post",
+        post: response.data,
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching post" });
+    }
+  });
+
+// Partially update a post
+app.post("/api/posts/:id", async (req, res) => {
+    console.log("called");
+    try {
+      const response = await axios.patch(
+        `${API_URL}/posts/${req.params.id}`,
+        req.body
+      );
+      console.log(response.data);
+      res.redirect("/");
+    } catch (error) {
+      res.status(500).json({ message: "Error updating post" });
+    }
+  });
+  app.post('/api/posts', async (req, res) => {
+    const response = await axios.post(`${API_URL}/create`, req.body);
+    console.log(response);
+    res.redirect("/");
+  })
+        app.listen(port, () => {
+          console.log(`App listening on port ${port}.`)
+        })
+
+      
